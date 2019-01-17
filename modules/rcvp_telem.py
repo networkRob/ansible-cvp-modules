0998bb546b5aed68fd4b0a98716f2d72dd98ea63
Rob Martin <robmartin@arista.com>
2019-01-17 13:03:34 -0600

    Re-Added duplicate check

--- modules/rcvp_telem.py
+++ modules/rcvp_telem.py
@@ -77,7 +77,7 @@ def parseSwIntfs(sw_ports):
                     tmp_intf = cur_intf[:cur_intf.find('/')]
                 else:
                     tmp_intf = cur_intf
-                if tmp_intf.find('Ethernet') == 0:
+                if tmp_intf.find('Ethernet') == 0 and tmp_intf not in tmp_list:
                     tmp_list.append(tmp_intf)
         tmp_dict['ports'] = tmp_list
         tmp_dict['serialNumber'] = switch['item']
